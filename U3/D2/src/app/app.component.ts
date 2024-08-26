b0594e749bcb5cba718408e72f3e0d4ebe05e669
GabrieleTerracciano <157364908+GabrieleTerracciano@users.noreply.github.com>
2024-08-26 17:38:13 +0200

    Commit Angular D2

---
+++ U3/D2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'D2';
+  content = 'Questa è la mia prima lezione di Angular';
+}
